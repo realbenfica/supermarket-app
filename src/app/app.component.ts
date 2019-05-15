82b7709ae61adf40335de5b637cc1bee8dee22c1
Alex Severino <alex@7lab.nl>
2019-05-15 17:16:20 +0200

    Add temp navigation and refactor recipe-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    recipe = true;
+    shopping= false;
+
+    toRecipes(info: string) {
+        if (info) {
+            this.recipe = true;
+            this.shopping = false;
+        }
+    }
+
+    toShopping(info: string) {
+        if (info) {
+            this.shopping = true;
+            this.recipe = false;
+        }
+    }
 }
