145a0c4dba98404696bdb9b8db437c8ed35869db
Alex Severino <alex@7lab.nl>
2019-05-16 17:42:49 +0200

    Adapt model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,32 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://live.staticflickr.com/5737/30622968353_35e06fcb52_b.jpg'
+            'Fruit Salad',
+            'This is a very delicious salad full of flavour!',
+            'https://live.staticflickr.com/5737/30622968353_35e06fcb52_b.jpg',
+            [
+                new Ingredient('kiwi', 3),
+                new Ingredient('bananas', 2),
+                new Ingredient('watermelon', 1),
+                new Ingredient('strawberries', 30)
+            ]
         ),
         new Recipe(
-            'A Delicious Recipe',
+            'A Tasty Breakfast',
             'This is simply so delicious',
-            'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889913_960_720.jpg'
+            'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889913_960_720.jpg',
+            [
+                new Ingredient('oranges', 1),
+                new Ingredient('jam', 1),
+                new Ingredient('pear', 1),
+                new Ingredient('bread', 2)
+            ]
         ),
     ];
 
