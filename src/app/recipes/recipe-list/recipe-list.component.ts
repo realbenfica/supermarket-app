45d964aca1c87c5375907199eb3b9bac74c463e0
Alex Severino <alex@7lab.nl>
2019-06-03 12:12:37 +0200

    Add clg to new recipe button

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,4 +15,8 @@ export class RecipeListComponent implements OnInit {
     ngOnInit() {
         this.recipes = this.recipeService.getRecipes();
     }
+
+    test() {
+        console.log("Ow noooo");
+    }
 }
