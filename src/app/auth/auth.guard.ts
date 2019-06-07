da14d22448d650836e61107f57e6efe033a909f6
realbenfica <alexgtr84@gmail.com>
2019-06-07 21:30:25 +0200

    Bug fix

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,23 +1,36 @@
-import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router, UrlTree } from "@angular/router";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { AuthService } from "./auth.service";
-import { map, take } from "rxjs/operators";
+import {
+    CanActivate,
+    RouterStateSnapshot,
+    ActivatedRouteSnapshot,
+    Router,
+    UrlTree
+} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) {}
 
-    constructor(private authService: AuthService,
-                private router: Router) {}
-
-    canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot
-        ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree>{
-        return this.authService.user.pipe(take(1), map(user => {
-            const isAuth = !! user;
-            if (isAuth) {
-                return true
-            }
-            return this.router.createUrlTree(['/auth'])
-        }))
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ):
+        | boolean
+        | UrlTree
+        | Promise<boolean | UrlTree>
+        | Observable<boolean | UrlTree> {
+        return this.authService.user.pipe(
+            take(1),
+            map(user => {
+                const isAuth = !!user;
+                if (isAuth) {
+                    return true;
+                }
+                return this.router.createUrlTree(['/auth']);
+            })
+        );
     }
 }
